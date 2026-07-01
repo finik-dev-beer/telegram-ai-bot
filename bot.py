a69626aa3c1406a6839fb65926a6c6ea9b9c7ed7
finik-dev-beer <vaporessmiagy@gmail.com>
2026-07-01 15:21:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,19 @@
-from telegram.ext import Application, CommandHandler
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
+
 from config import TOKEN
-from handlers import start
+from handlers import start, echo
 
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 print("Бот запущен!")
 
