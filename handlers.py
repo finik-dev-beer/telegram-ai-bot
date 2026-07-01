ee78a7ccd8c4cf7d5c9bd9c633376aaaf2e5f7d5
finik-dev-beer <vaporessmiagy@gmail.com>
2026-07-01 15:20:53 +0300

    Update greeting message in start function

--- handlers.py
+++ handlers.py
@@ -3,5 +3,12 @@ from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Привет! 👋\n\nЯ будущий ИИ-помощник."
+        "Привет! 👋 Я твой будущий ИИ-бот 🤖"
+    )
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    await update.message.reply_text(
+        f"Ты написал:\n\n{text}"
     )
